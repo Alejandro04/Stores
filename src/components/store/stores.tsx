6060034b72aa2b0f72abcce348fcf6d3b60156a1
Alejandro Roa <alejandro.roa@workiasolutions.com>
2023-06-29 15:39:06 -0400

    maquetacion y estilado: lista de tiendas, wip

--- src/components/store/stores.tsx
+++ src/components/store/stores.tsx
@@ -6,74 +6,156 @@ import { useGetStore } from '../../hooks/useGetStore';
 const queryClient = new QueryClient()
 
 const StoreList: React.FC = () => {
-    const [searchQuery, setSearchQuery] = useState('');
-    const [page, setPage] = useState(1);
-    const [limit, setLimit] = useState(10);
-  
-    const { data, isLoading, isError } = useGetStore({ searchQuery, page, limit });
-  
-    const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
-      setSearchQuery(event.target.value);
-      setPage(1);
-    };
-  
-    const handlePageChange = (newPage: number) => {
-      setPage(newPage);
-    };
-  
-    const handleLimitChange = (newLimit: number) => {
-      setLimit(newLimit);
-      setPage(1);
-    };
-  
-    if (isLoading) {
-      return <div>Loading...</div>;
-    }
-  
-    if (isError) {
-      return <div>Error occurred while fetching stores.</div>;
-    }
-  
-    return (
-      <div>
-        <div>
-          <input type="text" value={searchQuery} onChange={handleSearch} placeholder="Search..." />
-          <select value={limit} onChange={(e) => handleLimitChange(Number(e.target.value))}>
+  const [searchQuery, setSearchQuery] = useState('');
+  const [page, setPage] = useState(1);
+  const [limit, setLimit] = useState(10);
+
+  const { data, isLoading, isError } = useGetStore({ searchQuery, page, limit });
+
+  const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchQuery(event.target.value);
+    setPage(1);
+  };
+
+  const handlePageChange = (newPage: number) => {
+    setPage(newPage);
+  };
+
+  const handleLimitChange = (newLimit: number) => {
+    setLimit(newLimit);
+    setPage(1);
+  };
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (isError) {
+    return <div>Error occurred while fetching stores.</div>;
+  }
+
+  return (
+    <>
+      <section className="container px-4 mx-auto p-2">
+        <h1 className="text-2xl font-bold text-gray-500 pb-4">Store list</h1>
+        <div className="relative" style={{ display: 'flex', justifyContent: 'space-between' }}>
+          <input type="text" value={searchQuery} onChange={handleSearch} className="p-2 pl-8 rounded border border-gray-200 bg-gray-200 focus:bg-white focus:outline-none focus:ring-2 focus:ring-yellow-600 focus:border-transparent" placeholder="search..." defaultValue="" />
+          <svg className="w-4 h-4 absolute left-2.5 top-3.5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+          </svg>
+
+          <select
+            value={limit}
+            onChange={(e) => handleLimitChange(Number(e.target.value))}
+            className="p-2 pl-8 rounded border border-gray-200 bg-gray-200 focus:bg-white focus:outline-none focus:ring-2 focus:ring-yellow-600 focus:border-transparent"
+          >
             <option value={5}>5</option>
             <option value={10}>10</option>
             <option value={20}>20</option>
             <option value={50}>50</option>
           </select>
         </div>
-        {data && data.map((store, index) => (
-          <div key={store.id || index}>
-            <h3>name: {store.name}</h3>
-            <p>Location: {store.description}</p>
+        <div className="flex flex-col pt-4">
+          <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+            <div className="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
+              <div className="overflow-hidden border border-gray-200 dark:border-gray-700 md:rounded-lg">
+                <table className="min-w-full divide-y divide-gray-200 dark:divide-gray-700">
+                  <thead className="bg-gray-50 dark:bg-gray-800">
+                    <tr>
+                      <th scope="col" className="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                        Name
+                      </th>
+                      <th scope="col" className="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                        Description
+                      </th>
+                      <th scope="col" className="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                        Email
+                      </th>
+                      <th scope="col" className="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                        Phone
+                      </th>
+                      <th scope="col" className="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                        Address
+                      </th>
+                      <th scope="col" className="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                        City
+                      </th>
+                      <th scope="col" className="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                        State
+                      </th>
+                      <th scope="col" className="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                        Zip
+                      </th>
+                    </tr>
+                  </thead>
+                  <tbody className="bg-white divide-y divide-gray-200 dark:divide-gray-700 dark:bg-gray-900">
+                    {data && data.map((store, index) => (
+                      <tr key={store.id || index}>
+                        <td className="px-4 py-4 text-sm text-gray-500 dark:text-gray-300 whitespace-nowrap">
+                          {store.name}
+                        </td>
+                        <td className="px-4 py-4 text-sm text-gray-500 dark:text-gray-300 whitespace-nowrap">
+                          {store.description}
+                        </td>
+                        <td className="px-4 py-4 text-sm text-gray-500 dark:text-gray-300 whitespace-nowrap">
+                          {store.email}
+                        </td>
+                        <td className="px-4 py-4 text-sm text-gray-500 dark:text-gray-300 whitespace-nowrap">
+                          {store.phone}
+                        </td>
+                        <td className="px-4 py-4 text-sm text-gray-500 dark:text-gray-300 whitespace-nowrap">
+                          {store.address}
+                        </td>
+                        <td className="px-4 py-4 text-sm text-gray-500 dark:text-gray-300 whitespace-nowrap">
+                          {store.city}
+                        </td>
+                        <td className="px-4 py-4 text-sm text-gray-500 dark:text-gray-300 whitespace-nowrap">
+                          {store.state}
+                        </td>
+                        <td className="px-4 py-4 text-sm text-gray-500 dark:text-gray-300 whitespace-nowrap">
+                          {store.zip}
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
+            </div>
           </div>
-        ))}
-        <div>
-          <button disabled={page === 1} onClick={() => handlePageChange(page - 1)}>
+        </div>
+        <div className="flex items-center justify-center pt-4">
+          <button
+            className="py-2 px-4 bg-blue-500 text-white rounded-md mr-2"
+            disabled={page === 1}
+            onClick={() => handlePageChange(page - 1)}
+          >
             Previous
           </button>
-          <span>Page: {page}</span>
-          <button onClick={() => handlePageChange(page + 1)}>Next</button>
+          <span className="text-lg font-bold">Page: {page}</span>
+          <button
+            className="py-2 px-4 bg-blue-500 text-white rounded-md ml-2"
+            onClick={() => handlePageChange(page + 1)}
+          >
+            Next
+          </button>
         </div>
-      </div>
-    );
-  };
-  
-  const App: React.FC = () => {
-    return (
-      <QueryClientProvider client={queryClient}>
-        <StoreList />
-        <ReactQueryDevtools initialIsOpen={false} />
-      </QueryClientProvider>
-    );
-  };
-  
-  export default App;
-  
-  
-  
-  
-  
+      </section>
+    </>
+  );
+};
+
+const App: React.FC = () => {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <StoreList />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
+};
+
+export default App;
+
+
+
+
+
